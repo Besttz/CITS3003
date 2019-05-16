e8ef8df670f2769fa8530d4e967d6d6521b7f4e7
Tommy <tommyzhangchina@gmail.com>
2019-05-16 16:37:23 +0800

    转移到 Frag
    PerFrag 实现！

--- project/MAC_VERSIONS/fStart.glsl
+++ project/MAC_VERSIONS/fStart.glsl
@@ -2,23 +2,16 @@ varying vec2 texCoord;  // The third coordinate is always 0.0 and is discarded
 varying vec3 pos;
 varying vec4 normal;
 
-
-varying vec3 fN;
-varying vec3 fE;
-varying vec3 fL1;
-varying vec3 fL2;
-
-
-
 vec4 color;
 
 
 uniform sampler2D texture;
 uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
 uniform mat4 ModelView;
-uniform vec4 LightPosition;
 uniform float Shininess;
 uniform float texScale;
+uniform vec4 LightPosition;
+uniform vec4 LightPosition2;
 uniform vec3 Light1Rgb;
 uniform vec3 Light2Rgb;
 
@@ -28,31 +21,22 @@ void main()
     // Transfer from vStart
 
     // Unit direction vectors for Blinn-Phong shading calculation
-    // vec3 L = normalize( Lvec );   // Direction to the light source
-    // vec3 L2 = normalize( Lvec2 );   // Direction to the 2nd light source
+    vec3 fL1 = LightPosition.xyz - pos;
+    vec3 fL2 = LightPosition2.xyz - pos;     
 
-    // vec3 E = normalize( -pos );   // Direction to the eye/camera
-
-
-
-    vec3 N = normalize(fN); 
-    vec3 E = normalize(fE); 
-    vec3 L1 = normalize(fL1);
-    vec3 L2 = normalize(fL2);
-
+    // Transform vertex normal into eye coordinates (assumes scaling
+    // is uniform across dimensions)
+    vec3 N = normalize((ModelView * normal).xyz); 
+    vec3 E = normalize(- pos);  // Direction to the eye/camera
+    vec3 L1 = normalize(fL1);// Direction to the light source
+    vec3 L2 = normalize(fL2);// Direction to the 2nd light source
     vec3 H1 = normalize( L1 + E );  // Halfway vector
     vec3 H2 = normalize( L2 + E );  // Halfway vector
-
-
 
     // The length to the light from the vertex 
     float Ldis = length(fL1);
     float Ldis2 = length(fL2);
 
-
-    // Transform vertex normal into eye coordinates (assumes scaling
-    // is uniform across dimensions)
-    // vec3 N = normalize( (ModelView*vec4(normal, 0.0)).xyz );
 
     // Compute terms in the illumination equation
     vec4 ambient1 = vec4(Light1Rgb*AmbientProduct,1.0);
@@ -65,29 +49,26 @@ void main()
     vec4  diffuse1 = Kd1*vec4(DiffuseProduct,1.0);
     vec4  diffuse2 = Kd2*vec4(DiffuseProduct,1.0);
 
-    //+Kd2*vec4(DiffuseProduct,1.0)
 
     float Ks1 = pow( max(dot(N, H1), 0.0), Shininess );
     float Ks2 = pow( max(dot(N, H2), 0.0), Shininess );
 
-    // vec4  specular = Ks * vec4(SpecularProduct,1.0); // FOR G
     vec4 specular1 = Ks1 * vec4(SpecularProduct,1.0); // FOR H +
     vec4 specular2 = Ks2 * vec4(SpecularProduct,1.0);
 
     
     if (dot(L1, N) < 0.0 ) {
-	specular1 = vec4(0.0, 0.0, 0.0,1.0); //???
+	specular1 = vec4(0.0, 0.0, 0.0,1.0); 
     } 
     if (dot(L2, N) < 0.0 ) {
-	specular2 = vec4(0.0, 0.0, 0.0,1.0); //???
+	specular2 = vec4(0.0, 0.0, 0.0,1.0); 
     } 
 
     // globalAmbient is independent of distance from the light source
     vec3 globalAmbient = vec3(0.1, 0.1, 0.1);
 
     color.rgb = globalAmbient + ambient1.xyz + (diffuse1.xyz+ specular1.xyz)
-    *(1.0/(1.0+Ldis*Ldis))
-    +ambient2.xyz+diffuse2.xyz+ specular2.xyz;
+    *(1.0/(1.0+Ldis*Ldis))+ambient2.xyz+diffuse2.xyz+ specular2.xyz;
     color.a = 1.0;
 
     gl_FragColor = color * texture2D( texture, texCoord * texScale );
--- project/MAC_VERSIONS/vStart new.glsl
+++ project/MAC_VERSIONS/vStart new.glsl
@@ -3,7 +3,6 @@ attribute vec3 vNormal;
 attribute vec2 vTexCoord;
 
 varying vec2 texCoord;
-
 varying vec3 fL1;
 varying vec3 fL2;
 varying vec4 normal;
@@ -11,37 +10,18 @@ varying vec3 pos;
 
 uniform mat4 ModelView;
 uniform mat4 Projection;
-uniform vec4 LightPosition;
-uniform vec4 LightPosition2;
-
-varying vec3 fN;
-varying vec3 fE;
 
 
-//part 2
-// attribute vec4 boneIDs;
-// attribute vec4 boneWeights;
-// uniform mat4 boneTransforms[64];
 
 
 void main()
 {
-    vec4 vpos = vec4(vPosition, 1.0);
-
+    vec4 vpos =vec4(vPosition, 1.0);
     // Transform vertex position into eye coordinates
     pos = (ModelView * vpos).xyz;
-
-    // The vector to the light from the vertex    
-    // Lvec = LightPosition.xyz - pos;
-    // Lvec2 =LightPosition2.xyz - pos;
     normal = vec4(vNormal, 0.0);
+    // The vector to the light from the vertex    
 
-
-    fN = (ModelView * normal).xyz;
-    fE = - pos;
-    fL1 = LightPosition.xyz - pos;
-    fL2 = LightPosition2.xyz - pos;       
-       
 
     gl_Position = Projection * ModelView * vpos;
     texCoord = vTexCoord;
